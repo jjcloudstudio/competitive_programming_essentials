baa313df533cdfe5ee2f517aace87a61c80f0ac5
JJ <35040051+jjcloudstudio@users.noreply.github.com>
2023-01-06 10:38:56 +0530

    Update cost_to_paint_house.py

--- cost_to_paint_house.py
+++ cost_to_paint_house.py
@@ -1,1 +1,33 @@
+def colorHouse(N, red, green, blue, latest_color=None, count=0):
+    if count == N:
+        return 0
 
+    if latest_color==None:
+        return min(
+            red[count] + colorHouse(N, red, green, blue, 'r', count+1 ), 
+            green[count] + colorHouse(N, red, green, blue, 'g', count+1), 
+            blue[count] + colorHouse(N, red, green, blue, 'b', count+1) 
+        )
+    elif latest_color=='r':
+        return min(
+            green[count] + colorHouse(N, red, green, blue, 'g', count+1), 
+            blue[count] + colorHouse(N, red, green, blue, 'b', count+1) 
+        )
+    elif latest_color=='g':
+        return min(
+            red[count] + colorHouse(N, red, green, blue, 'r', count+1 ), 
+            blue[count] + colorHouse(N, red, green, blue, 'b', count+1) 
+        )
+    else:
+        return min(
+            red[count] + colorHouse(N, red, green, blue, 'r', count+1 ), 
+            green[count] + colorHouse(N, red, green, blue, 'g', count+1) 
+        )
+
+
+N = 3
+Red = [6, 5, 3]
+Green = [3, 1, 6]
+Blue =  [9, 4, 2]
+out = colorHouse(N, Red, Green, Blue)
+print("Minimum cost to paint houses: ", out)
