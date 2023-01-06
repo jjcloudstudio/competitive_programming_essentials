73d03e1ad4258c34c43a26a0ea49f26484d2327d
JJ <35040051+jjcloudstudio@users.noreply.github.com>
2023-01-06 10:39:58 +0530

    Update validPANCardNumbers.py

--- validPANCardNumbers.py
+++ validPANCardNumbers.py
@@ -1,1 +1,8 @@
+import re
+ 
+def get_valid_pan_number(par):
+    re_exp = r"[A-Z]{3}[ABCFGHLJPTK]{1}[A-Z]{1}[0-9]{4}[A-Z]{1}"
+    return re.findall(re_exp, par)
 
+par = "AABZA1111A AABAA1111AxMKOFM5336A"
+print(set(get_valid_pan_number(par)))
